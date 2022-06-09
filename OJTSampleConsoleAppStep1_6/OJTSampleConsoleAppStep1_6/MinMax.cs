2be65970a6fcb647e1ad4fce4b779dd616630ccf
kyoka-tsuda <kyoka-tsuda@kke.co.jp>
2022-06-09 10:07:28 +0900

    演習課題1-6

---
+++ OJTSampleConsoleAppStep1_6/OJTSampleConsoleAppStep1_6/MinMax.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OJTSampleConsoleAppStep1_6
+{
+    //最大値、最小値を求めるクラス
+    class MinMax
+    {
+        //最大値を取得するメソッド
+        public int Max(int[] n)
+        {
+            int max = 0; //最大値格納用の変数
+
+            for(int i=0; i < n.Length; i++)
+            {
+                //配列[i]に入っている値が、そのときのmaxより大きければmaxを更新する
+                if (n[i] > max)
+                {
+                    max = n[i];
+                }
+
+            }
+
+            return max; //最大値を返す
+
+        }
+
+        //最小値を取得するメソッド
+        public int Min(int[] n)
+        {
+            int min = 0;
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                if (i == 0)
+                {
+                    min = n[i]; //最初に入ってる値をminの初期値とする
+                }
+                else if (n[i] < min)
+                {
+                    min = n[i];
+                }
+
+            }
+
+            return min; //最小値を返す
+
+        }
+    }
+}
---
+++ OJTSampleConsoleAppStep1_6/OJTSampleConsoleAppStep1_6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OJTSampleConsoleAppStep1_6
+{
+    //【クラスとオブジェクト】
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 6-1
+             * 最大値と最小値を求めるクラス「MinMax」を作成し、5つの数の最大値・最小値を出力する。
+             */
+
+            MinMax minMax = new MinMax(); //クラス
+
+            int[] num = { 58, 67, 89, 74, 13 }; //数値が入った配列　→　MinMaxクラスの引数
+
+            Console.WriteLine("問6-1");
+            Console.WriteLine("最大値：" + minMax.Max(num)); //MinMaxクラスのMaxメソッドを使って最大値を得る
+            Console.WriteLine("最小値：" + minMax.Min(num)); //MinMaxクラスのMaxメソッドを使って最小値を得る
+
+            Console.WriteLine("続行するには何かキーを押してください . . .");
+            Console.ReadKey();
+        }
+    }
+}
